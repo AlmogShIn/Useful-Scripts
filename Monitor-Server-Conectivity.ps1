d1423cd00ed074c1f6d0807829508161246c255c
AlmogShIn <almog.shtaigmann@intel.com>
2022-12-04 11:30:52 +0200

    added try-catch

--- Monitor-Server-Conectivity.ps1
+++ Monitor-Server-Conectivity.ps1
@@ -4,18 +4,32 @@ $path = "C:\Users\ad_ashtaigm\Documents\log.csv"
 #Un commit for create the file
 #Add-Content -Path $path -Value 'Ping result, Time'
 
-while ($true){
+while ($true) {
     #Test conecticity
     $time = (get-date).ToUniversalTime()
-    $res = Test-Connection -ComputerName 'corp'-Quiet
+    try {
+        $res = Test-Connection -ComputerName 'corp'-Quiet    
+    }
+    catch {
+        Add-Content -Path $path -Value "Faild to ping, $($time)"
+    }
+    
+    $sleepTime = 90
     
     #If the ping faild
-    if($res -eq $false){
-        Add-Content -Path $path -Value "Disconnected, $($time)"
-        Write-Host "Dis"
+    if ($res -eq $false) {
+        try {
+            Add-Content -Path $path -Value "Disconnected, $($time)"
+            Write-Host "Dis"
+        
+            #If the ping fail, reduce the ping interval to 15 second
+            $sleepTime = 15
+        }
+        catch { Write-Warning "Faild to write log" }       
     }
-    else{
+    else {
+        if ($sleepTime -eq 15) { $sleepTime = 90 }
         Write-Host "Online - $($time)"
     }
-    Start-Sleep -Seconds 90
+    Start-Sleep -Seconds $sleepTime
 }
